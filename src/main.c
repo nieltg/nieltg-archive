33a918e5cd1a6cf0e87a8e4f6321cced04a0eefa
Daniel <nieltg@users.noreply.github.com>
2016-09-10 07:57:47 +0700

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,12 @@
+/* 
+ * NIM/Nama  : 13515071 / Daniel Pintara
+ * Tanggal   : Fri Sep 9, 2016
+ * Topik     : DFA
+ * Deskripsi : Implementasi DFA pada vending machine sederhana
+ */
+
+int main (int argc, char ** argv)
+{
+	return 0;
+}
+
