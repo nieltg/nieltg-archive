3819d07d92fad8ccb6d4aa6bf73a762f3ad4dcdd
Daniel <nieltg@users.noreply.github.com>
2016-09-18 14:11:21 +0700

    dfa: Change internal data structure
    Note:
    - Fix ability to verify DFA
    - Verified DFA is mandatory

--- src/dfa.c
+++ src/dfa.c
@@ -42,8 +42,14 @@ dfa_set (dfa_t * self, size_t state_id, size_t * dest)
 	assert (self != NULL);
 	assert (dest != NULL);
 	
-	memcpy (vector_id (self->data, state_id), dest,
-	        sizeof (size_t) * self->n_syms);
+	size_t cap = sizeof (size_t) * self->n_syms;
+	
+	size_t * buf = (size_t *) malloc (cap);
+	memcpy (buf, dest, cap);
+	
+	vector(self->data, state_id, size_t *) = buf;
+	
+	// Enlarge vector for validation
 	
 	self->is_dirty = true;
 	
@@ -62,7 +68,9 @@ dfa_get (dfa_t * self, size_t state_id, size_t sym_id)
 	assert (state_id < dfa_get_n_state (self));
 	assert (sym_id < dfa_get_n_symbol (self));
 	
-	size_t * sym = (size_t *) vector_id (self->data, state_id);
+	size_t * sym = vector(self->data, state_id, size_t *);
+	assert (sym != NULL);
+	
 	return sym[sym_id];
 }
 
@@ -94,14 +102,31 @@ dfa_verify (dfa_t * self)
 	
 	while ((i < len) && out)
 	{
-		if (vector(self->data, i, char *) == NULL)
+		if (vector(self->data, i, size_t *) == NULL)
 			out = false;
 		else
 			i++;
 	}
 	
-	self->is_dirty = out;
+	self->is_dirty = !out;
 	return out;
+}
+
+static void
+dfa_free_elmts (dfa_t * self)
+{
+	assert (self != NULL);
+	assert (self->data != NULL);
+	
+	size_t len = vector_get_size (self->data);
+	
+	for (size_t i = 0; i < len; i++)
+	{
+		size_t ** elmt = vector_id (self->data, i);
+		
+		free (*elmt);
+		*elmt = NULL;
+	}
 }
 
 void
@@ -109,7 +134,11 @@ dfa_free (dfa_t * self)
 {
 	if (self != NULL)
 	{
-		vector_clean (&self->data);
+		if (self->data != NULL)
+		{
+			dfa_free_elmts (self);
+			vector_clean (&self->data);
+		}
 		
 		free (self);
 	}
@@ -137,6 +166,7 @@ dfa_exec_t *
 dfa_exec_alloc (dfa_t * dfa, size_t initial)
 {
 	assert (dfa != NULL);
+	assert (!dfa->is_dirty);
 	assert (initial < dfa_get_n_state (dfa));
 	
 	dfa_exec_t * self = (dfa_exec_t *) malloc (sizeof (dfa_exec_t));
