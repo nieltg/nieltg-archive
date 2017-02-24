50190421d9310680d9fe615c259a23a5d3751508
Daniel <nieltg@users.noreply.github.com>
2017-02-24 13:01:22 +0700

    main: Use payload, animation

--- src/main.c
+++ src/main.c
@@ -1,42 +1,35 @@
 /**
- * Tugas OS
+ * Tugas Besar 1 IF2230 Operating System
  */
 
-#define COLOR_BLACK         (0)
-#define COLOR_BLUE          (1)
-#define COLOR_GREEN         (2)
-#define COLOR_CYAN          (3)
-#define COLOR_RED           (4)
-#define COLOR_MAGENTA       (5)
-#define COLOR_BROWN         (6)
-#define COLOR_LIGHT_GREY    (7)
-#define COLOR_DARK_GREY     (8)
-#define COLOR_LIGHT_BLUE    (9)
-#define COLOR_LIGHT_GREEN   (10)
-#define COLOR_LIGHT_CYAN    (11)
-#define COLOR_LIGHT_RED     (12)
-#define COLOR_LIGHT_MAGENTA (13)
-#define COLOR_LIGHT_BROWN   (14)
-#define COLOR_WHITE         (15)
+#include "anim.h"
+#include "payload.h"
+#include "vga.h"
 
-#define MAX_SCREEN  (2 * 80 * 25)
-
-int cursor = 0;
-
-void print (int color, char* str)
+void busy_wait (void)
 {
-	while (*str)
-	{
-		cursor %= MAX_SCREEN;
-		
-		putInMemory (0xB000, 0x8000 + cursor++, *str++);
-		putInMemory (0xB000, 0x8000 + cursor++, color);
-	}
+	unsigned i, j;
+
+	for (i = 0; i < 32767; i++)
+		for (j = 0; j < 40; j++);
 }
 
 int kernel_main (void)
 {
-	print (COLOR_WHITE, "Hello World! ");
-	
+	vga_cursor_hide ();
+
+	vga_screen_write (0, 0, payload_bitmap);
+
+	anim_init ();
+	anim_render ();
+
+	while (1)
+	{
+		busy_wait ();
+
+		anim_next ();
+		anim_render ();
+	}
+
 	return 0;
 }
