74ba789e28c572d76b3cc90d19e661784f67b872
Daniel <nieltg@users.noreply.github.com>
2017-02-25 12:10:52 +0700

    timer: Add PIT driver

---
+++ src/timer.c
@@ -0,0 +1,56 @@
+/**
+ * Tugas Besar 1 IF2230 Operating System
+ */
+
+#include "anim.h"
+
+#include "timer.h"
+
+void _timer_irq_handler (void)
+{
+	if (anim_is_idle ())
+		reboot ();
+
+	anim_next ();
+	anim_render ();
+
+	outb (0x20, 0x20);
+}
+
+void timer_init (void)
+{
+#asm
+	cli
+#endasm
+
+	/*
+	 * http://inglorion.net/documents/tutorials/x86ostut/keyboard
+	 * http://osdever.net/bkerndev/Docs/pit.htm
+	 * http://wiki.osdev.org/Interrupt_Vector_Table
+	 * http://wiki.osdev.org/Programmable_Interval_Timer
+	 */
+
+	outb (0x43, 00110100b);
+
+	outb (0x40, 0xFF);
+	outb (0x40, 0xFF);
+
+#asm
+	push    ds
+	push    *0x0
+	pop     ds
+	mov     [0x20], #_timer_init_irq
+	mov     [0x22], cs
+	sti
+	pop     ds
+	jmp     _timer_init_end
+_timer_init_irq:
+	cli
+	pusha
+	call    __timer_irq_handler
+	popa
+	sti
+	iret
+_timer_init_end:
+#endasm
+}
---
+++ src/timer.h
@@ -0,0 +1,10 @@
+/**
+ * Tugas Besar 1 IF2230 Operating System
+ */
+
+#ifndef __TIMER_H__
+#define __TIMER_H__ 1
+
+void timer_init (void);
+
+#endif
